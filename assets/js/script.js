c46cd3e943d079120504c6dc3212cde2364dfbeb
SJohnRose <shamini.johnrose@gmail.com>
2022-12-09 18:17:23 +1100

    Created page layout in index.html and style.css files

---
+++ assets/js/script.js
@@ -0,0 +1,4 @@
+var mainEl = document.querySelectorAll("main");
+var scoreEl = document.querySelector("score-section");
+var quizEl = document.querySelector("main-quiz-section");
+var timeEl = document.querySelector("time-section");
